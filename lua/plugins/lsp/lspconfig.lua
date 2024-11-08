b2166593e00258a2d87c06d8015b0d98da1db8e3
svadotwave <checox.ss7@gmail.com>
2024-11-08 16:22:35 -0400

    style(lsp): formatter files lsp

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -1,64 +1,64 @@
 return {
-	"neovim/nvim-lspconfig",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"jose-elias-alvarez/null-ls.nvim",
-	},
-	config = function()
-		local on_attach = function(client, bufnr)
-			local bufopts = { noremap = true, silent = true, buffer = bufnr }
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-			vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "jose-elias-alvarez/null-ls.nvim",
+  },
+  config = function()
+    local on_attach = function(client, bufnr)
+      local bufopts = { noremap = true, silent = true, buffer = bufnr }
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
 
-			-- Aseguramos que null-ls pueda manejar el formateo si está presente
-			if client.name == "null-ls" then
-				client.resolved_capabilities.document_formatting = true
-			end
-		end
+      -- Aseguramos que null-ls pueda manejar el formateo si está presente
+      if client.name == "null-ls" then
+        client.resolved_capabilities.document_formatting = true
+      end
+    end
 
-		-- Configuración automática de los servidores instalados por Mason
-		require("mason-lspconfig").setup_handlers({
-			-- Configuración por defecto para todos los servidores
-			function(server_name)
-				require("lspconfig")[server_name].setup({
-					on_attach = on_attach,
-				})
-			end,
-			-- Configuración personalizada para el servidor `lua_ls`
-			["lua_ls"] = function()
-				require("lspconfig").lua_ls.setup({
-					on_attach = on_attach,
-					settings = {
-						Lua = {
-							diagnostics = {
-								globals = { "vim" },
-							},
-						},
-					},
-				})
-			end,
-		})
+    -- Configuración automática de los servidores instalados por Mason
+    require("mason-lspconfig").setup_handlers({
+      -- Configuración por defecto para todos los servidores
+      function(server_name)
+        require("lspconfig")[server_name].setup({
+          on_attach = on_attach,
+        })
+      end,
+      -- Configuración personalizada para el servidor `lua_ls`
+      ["lua_ls"] = function()
+        require("lspconfig").lua_ls.setup({
+          on_attach = on_attach,
+          settings = {
+            Lua = {
+              diagnostics = {
+                globals = { "vim" },
+              },
+            },
+          },
+        })
+      end,
+    })
 
-		-- Formateo automático al guardar
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			group = vim.api.nvim_create_augroup("LspFormatting", { clear = true }),
-			pattern = "*",
-			callback = function()
-				vim.lsp.buf.format({ async = false })
-			end,
-		})
+    -- Formateo automático al guardar
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = vim.api.nvim_create_augroup("LspFormatting", { clear = true }),
+      pattern = "*",
+      callback = function()
+        vim.lsp.buf.format({ async = false })
+      end,
+    })
 
-		-- Atajo para formatear el código manualmente
-		vim.keymap.set("n", "<leader>f", function()
-			vim.lsp.buf.format({ async = true })
-		end, { desc = "Format current buffer" })
+    -- Atajo para formatear el código manualmente
+    vim.keymap.set("n", "<leader>f", function()
+      vim.lsp.buf.format({ async = true })
+    end, { desc = "Format current buffer" })
 
-		-- local lspconfig = require("lspconfig")
-		-- Configuración básica de servidores LSP
-		-- lspconfig.pyright.setup{}  -- Python
-		-- lspconfig.tsserver.setup{} -- JavaScript/TypeScript
-		-- lspconfig.lua_ls.setup{}
-	end,
+    -- local lspconfig = require("lspconfig")
+    -- Configuración básica de servidores LSP
+    -- lspconfig.pyright.setup{}  -- Python
+    -- lspconfig.tsserver.setup{} -- JavaScript/TypeScript
+    -- lspconfig.lua_ls.setup{}
+  end,
 }
--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,19 +1,19 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				-- ensure_installed = {
-				--   "lua_ls",
-				--   "pyright"
-				-- }, -- Lista de LSPs a instalar automáticamente
-			})
-		end,
-	},
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        -- ensure_installed = {
+        --   "lua_ls",
+        --   "pyright"
+        -- }, -- Lista de LSPs a instalar automáticamente
+      })
+    end,
+  },
 }
