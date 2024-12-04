f254587e111ad175dee0cc3d987711c4d19d2258
svadotwave <checox.ss7@gmail.com>
2024-12-04 15:31:55 -0400

    chore(init): initialize neovim configuration

---
+++ lua/core/init.lua
@@ -0,0 +1,4 @@
+require("core.options")  -- Configurar las opciones globales primero.
+require("core.lazy")     -- Cargar y configurar plugins.
+require("core.keymaps")  -- Definir keymaps que podrían depender de plugins.
+require("core.autocmds") -- Configurar autocmds, basados en el resto de la configuración.
