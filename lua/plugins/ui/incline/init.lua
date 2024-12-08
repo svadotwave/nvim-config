e16866f3b114fb89ecd3ebc749ec6b0885814e5c
svadotwave <checox.ss7@gmail.com>
2024-12-08 16:50:36 -0400

    feat: add incline

---
+++ lua/plugins/ui/incline/init.lua
@@ -0,0 +1,9 @@
+return {
+  "b0o/incline.nvim",
+  event = "BufReadPre", -- Load this plugin before reading a buffer
+  priority = 1200, -- Set the priority for loading this plugin
+  config = function()
+    require("incline").setup({
+    })
+  end,
+}
